3ff4a6c3f166ae9a18108a9b108e656ee7e99290
jaeho0613 <ywm2004@gmail.com>
2022-12-28 17:32:57 +0900

    part2 - ch05 - recoil 2 끝

--- part2/ch05/src/App.js
+++ part2/ch05/src/App.js
@@ -3,14 +3,16 @@ import { RecoilRoot } from "recoil";
 import FontButton from "./components/RecoilExample/tutorial/FontButton";
 import Text from "./components/RecoilExample/tutorial/Text";
 import CharacterCounter from "./components/RecoilExample/tutorial/CharacterCounter";
+import TodoList from "./components/RecoilExample/todo/TodoList";
 
 function App() {
   return (
     <div className="App">
       <RecoilRoot>
-        <FontButton />
-        <Text />
-        <CharacterCounter />
+        {/*<FontButton />*/}
+        {/*<Text />*/}
+        {/*<CharacterCounter />*/}
+        <TodoList />
       </RecoilRoot>
     </div>
   );
--- part2/ch05/src/components/RecoilExample/todo/TodoList.jsx
+++ part2/ch05/src/components/RecoilExample/todo/TodoList.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRecoilValue } from "recoil";
-import { todoListState } from "./TodoStore";
+import { filteredTodoListState, todoListState } from "./TodoStore";
+import TodoItem from "./TodoItem";
+import TodoListCreator from "./TodoListCreator";
+import TodoListFilters from "./TodoListFilters";
+import TodoListStats from "./TodoListStats";
 
 function TodoList() {
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
+
+  // useEffect(() => {
+  //   console.log(`todoList : ${JSON.stringify(todoList)}`);
+  // }, [todoList]);
 
   return (
     <>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
-      {/*<TodoItemCreator />*/}
+      <TodoListStats />
+      <TodoListFilters />
+      <TodoListCreator />
 
       {todoList.map((todoItem) => (
         <TodoItem key={todoItem.id} item={todoItem} />
