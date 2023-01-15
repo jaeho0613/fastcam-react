791ca20381ef90d26d6b85d53a1fdd8eaf602317
jaeho0613 <ywm2004@gmail.com>
2023-01-15 23:35:14 +0900

    [재호] Part3 - recoil todo list - 03 완료

--- part3/recoil-todo-list/src/index.tsx
+++ part3/recoil-todo-list/src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RecoilRoot} from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <React.Suspense fallback={<p>Loading..</p>}>
+      <App/>
+    </React.Suspense>
+  </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
